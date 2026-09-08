569a4559d2ceff6dc2a9291709c900128e4e7a03
nandoribersbr <nandoribers2@gmail.com>
2026-09-08 17:28:36 -0400

    fix: use available compose material icons version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.9.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
     implementation("androidx.compose.material3:material3:1.3.2")
-    implementation("androidx.compose.material:material-icons-extended:1.9.1")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
 
     implementation("androidx.camera:camera-core:$cameraX")
     implementation("androidx.camera:camera-camera2:$cameraX")
