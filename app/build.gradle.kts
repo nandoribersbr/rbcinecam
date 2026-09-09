bb35e0807380abb8b9f14c563c03cfe16122ee64
nandoribersbr <nandoribers2@gmail.com>
2026-09-09 07:45:07 -0400

    chore: bump RB CineCam to 0.9 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,8 @@ android {
         applicationId = "br.com.rb8digital.rbcinecam"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.8.0-alpha"
-
+        versionCode = 9
+        versionName = "0.9.0-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
